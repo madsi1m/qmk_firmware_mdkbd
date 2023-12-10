cfc58adb516529dd85097a1684d5e7e4780bd4a8
Michael D'Silva <michael.dsilva@outlook.com>
2023-12-11 09:13:08 +1100

    fix mdkbd2

--- keyboards/mdkbd2/keymaps/default/keymap.c
+++ keyboards/mdkbd2/keymaps/default/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_all(
+    [0] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,  KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, KC_BTN1, KC_BTN3, KC_BTN2, KC_SPC,  KC_INS, KC_HOME, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT		
     ),
 
-    [1] = LAYOUT_all(
+    [1] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, QK_BOOT,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,  KC_MINS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,  KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -17,3 +17,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RCTL, KC_RGUI, KC_RALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,  KC_DOT,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS	
     )
 };
+
+/*
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  //debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
+*/
+
--- keyboards/mdkbd2/rules.mk
+++ keyboards/mdkbd2/rules.mk
@@ -1,4 +1,1 @@
 #see info.json
-
-PS2_MOUSE_ENABLE = yes
-PS2_USE_USART = yes
